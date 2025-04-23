0ae7f55cbad28b48f2de5e7bef282528c715449d
junhyung8795 <junhyung8795@naver.com>
2025-04-23 11:30:03 +0900

    디스크 컨트롤러 문제 복습 및 오답노트. Count변수와 index변수 두 가지의 용도와 필요성 주석 추가

--- src/programmers/DiskController.java
+++ src/programmers/DiskController.java
@@ -3,138 +3,97 @@ package programmers;
 import java.util.*;
 
 //class DiskController {
-//
-//    public int solution(int[][] jobs) {
-//        int answer = 0;
-//        Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
-//        PriorityQueue<Job> pq = new PriorityQueue<>(new Comparator<Job>(){
-//            @Override
-//            public int compare(Job o1, Job o2){
-//                if (o1.spendTime != o2.spendTime){
-//                    return o1.spendTime - o2.spendTime;
-//                } else if(o1.startTime != o2.startTime){
-//                    return o1.startTime - o2.startTime;
-//                } else {
-//                    return o1.num - o2.num;
-//                }
-//            }
-//        });
-//        int time = 0;
-//        pq.offer(new Job(0,jobs[0][0],jobs[0][1]));
-//        int[][] timeLine = new int[jobs.length][2];
-//        int index = 0;
-//
-//        while(pq.size() > 0){
-//            Job job = pq.poll();
-//            if (job.startTime > time){
-//
-//                time = job.startTime + job.spendTime;
-//
-//            } else{
-//
-//                time += job.spendTime;
-//
-//            }
-//            boolean put = false;
-//            timeLine[job.num][0] = job.startTime;
-//            timeLine[job.num][1] = time;
-//
-//            if (index + 1 != jobs.length){
-//                for (int i = index + 1; i < jobs.length; i++){
-//                    if (jobs[i][0] <= time){
-//                        pq.offer(new Job(i, jobs[i][0], jobs[i][1]));
-//                        index = i;
-//
-//                        put = true;
-//                    } else {
-//                        break;
-//                    }
-//                }
-//                if (!put){
-//                    index += 1;
-//                    pq.offer(new Job(index, jobs[index][0], jobs[index][1]));
-//                }
-//            }
-//
-//
-//        }
-//        int sum = 0;
-//        for(int i = 0; i < jobs.length; i++){
-//            sum += timeLine[i][1] - timeLine[i][0];
-//        }
-//        answer = sum / timeLine.length;
-//
-//        return answer;
-//    }
-//    public static class Job{
-//        int num;
-//        int startTime;
-//        int spendTime;
-//        public Job(int num, int startTime, int spendTime){
-//            this.num = num;
-//            this.startTime = startTime;
-//            this.spendTime = spendTime;
-//        }
-//        @Override
-//        public String toString() {
-//            return "Job{" +
-//                    "num=" + num +
-//                    ", startTime=" + startTime +
-//                    ", spendTime=" + spendTime +
-//                    '}';
-//        }
-//    }
-//}
-
-import java.util.*;
-
 class DiskController {
 
     public int solution(int[][] jobs) {
         int answer = 0;
-        int time = 0; // 현재 시간
-        int jobsIdx = 0; // jobs 배열 인덱스
-        int count = 0; // 완료된 작업 수
+        //맨처음 풀이는 처음에 요소를 하나 집어넣는 과정이 잘못됐다.
+        //애초에 0초가 됐을 때 큐에 요소가 하나만 들어가리란 보장이 없다.
+        //0초에 3,4개의 작업이 들어갈 수 있다는 소리.
+        //시작부터 time보다 작거나 같은 모든 작업을 큐에 집어넣어줘야한다.
+        Comparator<Job> cp = new Comparator<>(){
+            @Override
+            public int compare(Job a, Job b){
+                if (a.spendTime != b.spendTime){
+                    return a.spendTime - b.spendTime;
+                } else if(a.startTime != b.startTime){
+                    return a.startTime - b.startTime;
+                } else{
+                    return a.num - b.num;
+                }
+            }
+        };
 
-        Arrays.sort(jobs, (a, b) -> a[0] - b[0]); // 요청 시간 기준 정렬
+        PriorityQueue<Job> pq = new PriorityQueue<>(cp);
 
-        PriorityQueue<Job> pq = new PriorityQueue<>(
-                (a, b) -> a.spendTime - b.spendTime
-        );
+        //먼저 jobs가 시간 순서대로 넣어졌다는 보장이 없으므로 정렬을 시켜줘야한다.
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
 
-        while (count < jobs.length) {
-            // 현재 시간 이전에 들어온 job들 PQ에 삽입
-            while (jobsIdx < jobs.length && jobs[jobsIdx][0] <= time) {
-                pq.offer(new Job(jobsIdx, jobs[jobsIdx][0], jobs[jobsIdx][1]));
-                jobsIdx++;
+        //현재 검토가 완료된 위치 index.
+        int index = -1;
+        int count = 0;
+        //현재 진행된 시간
+        int time = 0;
+
+        //검토하려는 index위치가 jobs의 길이보다는 작아야한다.
+        //만약 같아지면 모든 작업의 검토가 완료됐다는 의미
+        while (count < jobs.length){
+            //먼저 긁어와야한다.
+            //그런데 이 긁어오는 과정은 하나의 작업이 끝날때마다 이뤄져야한다,
+            for(int i = index + 1; i < jobs.length; i++){
+
+                if (jobs[i][0] <= time){
+                    pq.offer(new Job(i, jobs[i][0], jobs[i][1]));
+                    index = i;
+                    // System.out.print("인덱스 검사중: " + i + " ");
+                } else{
+                    break;
+                }
+            }
+            //그런데 만약에 time보다 이전에 신청된 작업이 없다면?
+            //time이후의 가장 가까운 시일내 신청된 작업의 시간을 보고 그 시간보다 작거나 같은
+            //모든 작업을 넣어줘야한다.
+
+
+            //그다음에 pq에 있는 작업들을 하나씩 꺼내고 time을 갱신해주고
+            //time이 갱신됐으면 time보다 작거나 같은 작업들을 모두 pq에 넣어주고
+            //만약 pq가 다 비었으면? 또 긁어주는 루프. 바깥 루프에서 또 긁어서 Pq에 넣어준다.
+
+            if (!pq.isEmpty()){
+                // System.out.print("큐에 들어잇는 것." + pq + " ");
+                Job popped = pq.poll();
+                count ++;
+                // System.out.print(time + ", ");
+                // System.out.print(popped.startTime + ", ");
+                // System.out.print(popped.spendTime + ", ");
+
+                if (time >= popped.startTime){
+                    time += popped.spendTime;
+                } else {
+                    time = popped.startTime;
+                    time += popped.spendTime;
+                }
+                answer += time - popped.startTime;
+                // System.out.print(time);
+                // System.out.print("index = " + index);
+                // System.out.println();
+            } else{
+                time = jobs[index + 1][0];
             }
 
-            if (!pq.isEmpty()) {
-                Job job = pq.poll();
-                time = Math.max(time, job.startTime); // 작업 시작시간 갱신
-                time += job.spendTime;
-                answer += (time - job.startTime); // 요청부터 종료까지의 시간
-                count++;
-            } else {
-                // 대기할 job이 없는 경우: 다음 작업의 요청시간으로 점프
-                time = jobs[jobsIdx][0];
-            }
         }
-
-        return answer / jobs.length;
+        answer = answer / jobs.length;
+        return answer;
     }
-
-    public static class Job {
+    public static class Job{
         int num;
         int startTime;
         int spendTime;
-
-        public Job(int num, int startTime, int spendTime) {
+        public Job(int num, int startTime, int spendTime){
             this.num = num;
             this.startTime = startTime;
             this.spendTime = spendTime;
         }
-
         @Override
         public String toString() {
             return "Job{" +
@@ -145,3 +104,13 @@ class DiskController {
         }
     }
 }
+
+
+//count변수가 중요하다.
+//index + 1 <= jobs.length를 하면 pq에 몾든 요소를 집어넣기만 하면 다 끝나버린다.
+//하지만 while안에서는 if문으로 Poll을 한다. 매번 요소를 뺄때마다 새로운 요소를 긁어와서 가장 우선순위가 높은 요소를 꺼내올 수 있도록 해야해서
+//그런데 Index로 루프종료를 하면 pq에 만약 마지막에 2,3개 넣어서 모든 요소를 Pq에 넣으면
+// 한개만 Poll을 한 후에 바로 index + 1 == jobs.length가 돼어서 pq에있는 모든 요소들을 털 수가 없다.
+//index도 필요하고 검사완료된 요소 count변수도 필요하다.
+//count: 완료된 요소를 정확히 세기 위해서 필요. index보다 이 조건이 더 우선이다.
+//index: 시작시간 순서대로 정렬된 jobs에서 요소를 시간이 제일 작은 놈들부터 검사하는데 어디까지 검사했는지 알기 위해서 필요.
